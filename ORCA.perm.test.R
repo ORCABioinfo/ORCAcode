560dcf947b15690bd2543e53e44879f3fcc1a3ba
ORCABioinfo <yotsa@YOTSAWATs-MacBook-Air.local>
2014-08-18 13:27:34 +0100

    ORCA code revised with comments
    Added comments in ORCA.perm.test.R to aid the clarity of the code

--- ORCA.perm.test.R
+++ ORCA.perm.test.R
@@ -1,3 +1,31 @@
+############################################################################################################
+## OVER-REPRESENTATION OF CORRELATION (ORCA): A METHOD FOR IDENTIFYING ASSOCIATIONS BETWEEN VARIABLE SETS ## 
+############################################################################################################
+
+##############################################################################################
+## 																							##
+## The input data for ORCA can be any numerical data that have enough data points for  		##
+## correlation coefficient calculation. The actual analysis uses two input data types: 		##
+## 		1. Correlation matrix of all the variables calculated from the raw numerical data   ##
+## 		2. Groupings or categories of the variables in the correlation matrix				##
+## 																							##
+## The input data for the examples we provided in the manuscripts are as follow:			##
+## 		1. Drug sensitivity dataset															##
+##			1.1 Correlation matrix between growth inhibition at 50% concentrations 			##
+## 				(GI50 values) of 116 chemotherapeutic drugs on 58 cell lines from NCI60 		##
+##				cell panle. 																	##
+##			1.2 Groupings are the mechanisms of action of the drugs.							##
+##		2. microRNA datasets																	##
+## 			2.1 Correlation matrix between baseline expression of microRNAs on 58 cell  		## 
+## 				lines from NCI60 cell panel. 												##
+## 			2.2 Groupings are the microRNA clusters based on miRConnect study 				##
+## 				(Hua et al. 2011).															##
+## 																							##
+## 			The output from ORCA is a hypergeometric p-value matrix calculated by 			##
+## 			hypergeometric test between every group-pair. 									##
+## 																							##
+##############################################################################################
+
 ## drug sensitivity data ## 
 load('drug.sensitivity')
 load('no.member')
